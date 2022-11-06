2a4f68dde6e821789546397ee03c144c8c947e63
V-theG <bmboxx@icloud.com>
2022-11-06 11:34:57 +0300

    Initial commit

---
+++ Controllers/HomeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HomeViewController.swift
+//  NetflixUIKit
+//
+//  Created by V K on 06.11.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "hello"
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        40
+//    }
+}
---
+++ Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixUIKit
+//
+//  Created by V K on 06.11.2022.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
